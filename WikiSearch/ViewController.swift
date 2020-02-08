7915919ec2bf0c947f2d8d4e444a9ac9b8234615
pramodie <adpramodie@gmail.com>
2020-02-08 17:40:35 +0530

    Initial Commit

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by pramodie athauda on 2/8/20.
+//  Copyright © 2020 pramodie athauda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
